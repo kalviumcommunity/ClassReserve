6f8be5fa4e1fad61b92611bc5c3dac2d25858696
nikiljos <nikhiljose12345@gmail.com>
2023-11-15 23:52:55 +0530

    fix: add override keyword

--- modules/course.h
+++ modules/course.h
@@ -21,14 +21,14 @@ class Course{
 
 class Bachelors: public Course{
     public:
-    void printDetail(){
+    void printDetail() override {
         cout<<"\n"<<this->name<<" is a Bachelors course that spans over "<<this->years<<" years.\nThe minimum eligibility for this course is 12th grade.\n";
     }
 };
 
 class Masters: public Course{
     public:
-    void printDetail(){
+    void printDetail() override {
         cout<<"\n"<<this->name<<" is a Masters course that spans over "<<this->years<<" years.\nThe minimum eligibility for this course is a bachelor's degree.\n";
     }
 };
